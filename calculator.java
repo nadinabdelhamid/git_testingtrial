2ef3b18027c6be62716eab44336e8b29b56a2a22
NadinAbdelhamid <nadine.sayed@vodafone.com>
2020-11-04 09:04:42 +0200

    First Commit After Push/ Adding Fibonacci

--- calculator.java
+++ calculator.java
@@ -1,47 +1,89 @@
+import java.math.BigInteger;
 import java.util.Scanner;
-
 
 public class Calculator {
 
-	 public static void main(String[] args) {
+	public static BigInteger fibonacci(int n) {
 
-	        Scanner reader = new Scanner(System.in);
-	        System.out.print("Enter two numbers: ");
+		if (n <= 1)
+			return BigInteger.valueOf(n);
+
+		BigInteger previous = BigInteger.ZERO, next = BigInteger.ONE, sum;
+
+		for (int i = 2; i <= n; i++) {
+
+			sum = previous;
+			previous = next;
+			next = sum.add(previous);
+		}
+
+		return next;
+	}
+
+	public static void Calculator() {
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter two numbers: ");
+
+		// nextDouble() reads the next double from the keyboard
+		double first = reader.nextDouble();
+		double second = reader.nextDouble();
+
+		System.out.print("Enter an operator (+, -, *, /): ");
+		char operator = reader.next().charAt(0);
+
+		double result;
+
+		switch (operator) {
+		case '+':
+			result = first + second;
+			break;
+
+		case '-':
+			result = first - second;
+			break;
+
+		case '*':
+			result = first * second;
+			break;
+
+		case '/':
+			result = first / second;
+			break;
+		// operator doesn't match any case constant (+, -, *, /)
+		default:
+			System.out.printf("Error! operator is not correct");
+			return;
+		}
+
+		System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter 1 for Calculator, 2 for Fibonacci: ");
+
+		int input = reader.nextInt();
+		
+		if (input == 1)
+			Calculator();
+		
+		
+		else if (input == 2){
+	        System.out.print("Enter n to be calculated: ");
 
 	        // nextDouble() reads the next double from the keyboard
-	        double first = reader.nextDouble();
-	        double second = reader.nextDouble();
+	        int n = reader.nextInt();
+	        
+	        for (int i = 0; i <= n; i++) {
 
-	        System.out.print("Enter an operator (+, -, *, /): ");
-	        char operator = reader.next().charAt(0);
-
-	        double result;
-
-	        switch(operator)
-	        {
-	            case '+':
-	                result = first + second;
-	                break;
-
-	            case '-':
-	                result = first - second;
-	                break;
-	            
- case '*':
-	                result = first * second;
-	                break;
-
-	            
- case '/':
-	                result = first / second;
-	                break;
-	            // operator doesn't match any case constant (+, -, *, /)
-	            default:
-	                System.out.printf("Error! operator is not correct");
-	                return;
+	            BigInteger val = fibonacci(i);
+	            System.out.println(val);
 	        }
-
-	        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
-	    }
-
+		}
+		
+		else
+			System.out.println("Wrong Choice.");
+	}
 }
